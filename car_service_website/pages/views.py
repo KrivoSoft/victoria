05021efdd2a576d558e70333ce045f48e2d3dc8f
vladimir <pylin.vs@gmail.com>
2023-05-27 22:10:31 +0500

    Created classed-based representation.

--- car_service_website/pages/views.py
+++ car_service_website/pages/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MainPage(TemplateView):
+    template_name = "pages/MainPage.html"
+
