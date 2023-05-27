c17850e9577669b133ab2a1758271241a3c539a5
vladimir <pylin.vs@gmail.com>
2023-05-27 22:14:55 +0500

    Project renamed.

--- car_service_website/victoria/asgi.py
+++ car_service_website/victoria/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for car_service_website project.
+ASGI config for victoria project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_service_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'victoria.settings')
 
 application = get_asgi_application()
--- car_service_website/victoria/urls.py
+++ car_service_website/victoria/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for car_service_website project.
+URL configuration for victoria project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
