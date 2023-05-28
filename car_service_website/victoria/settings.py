58f6b9ff5f9df252b9ddb9203e0c0805d9d8cbe8
vladimir <pylin.vs@gmail.com>
2023-05-28 11:40:57 +0500

    Connected with bootstrap library.

--- car_service_website/victoria/settings.py
+++ car_service_website/victoria/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages'
+    'pages',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
